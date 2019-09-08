7cfc005d6ec13a79c62940ca2cdf501a3c22139a
Zachary Murray <zacharymurray@hotmail.com>
2019-09-08 14:47:59 +1000

    Front end js dependencies working

--- routing.js
+++ routing.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.sendFile("views/index.html", { root: __dirname });
 });
+
+// Expose client side JS modules
+router.use('/inc/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+router.use('/inc/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 module.exports = router;
