03b91287cb1904905fd7b4fdb3a17def134fa8c6
Zachary Murray <zacharymurray@hotmail.com>
2019-09-08 14:59:05 +1000

    Structure improvements

--- routing.js
+++ routing.js
@@ -6,7 +6,10 @@ router.get('/', (req, res) => {
   res.sendFile("views/index.html", { root: __dirname });
 });
 
-// Expose client side JS modules
+//Expose css
+router.use('/css', express.static(__dirname + '/css/'));
+
+// Expose client side modules
 router.use('/inc/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 router.use('/inc/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
