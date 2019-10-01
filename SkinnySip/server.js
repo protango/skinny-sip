e7835564b1f05446964759e55e116fb1ff09a4d8
Zachary Murray <zacharymurray@hotmail.com>
2019-10-01 14:39:02 +1000

    Ensure views path is set correctly

--- SkinnySip/server.js
+++ SkinnySip/server.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+const path = require('path')
 const routes = require('./routing');
 
 // Constants
-const PORT = 80;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
